d86c7d0343859e9b2982cdbec98af0186714f91a
Dimasik <dimasizrus228@yandex.ru>
2026-04-09 22:16:01 +0300

    meowmeow

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello from repository 1!')
+from dotenv import load_dotenv
+import os
+
+def print_author():
+	load_dotenv()
+	author = os.getenv("AUTHOR")
+	print(f"Автор проекта: {author}")
+
+print_author()
